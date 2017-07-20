e7fec004bcaac4594ae86dd38343f87067230d05
Nate Bosch <nbosch@google.com>
2017-07-20 09:24:41 -0700

    Add back library statement

--- lib/source_gen.dart
+++ lib/source_gen.dart
@@ -2,6 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+library source_gen;
+
 export 'src/builder.dart';
 export 'src/constants.dart' show ConstantReader;
 export 'src/generator.dart';
