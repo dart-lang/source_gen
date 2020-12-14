4419602a68c6411f3ef927df7861a7ba70be8254
Jacob MacDonald <jakemac@google.com>
2020-12-14 09:06:42 -0800

    fix bug in early bailout case - check part files for annotations also

--- source_gen/test/src/comment_generator.dart
+++ source_gen/test/src/comment_generator.dart
@@ -3,6 +3,7 @@
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:analyzer/dart/element/element.dart';
+import 'package:build/src/builder/build_step.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Generates a single-line comment for each class
@@ -34,4 +35,12 @@ class CommentGenerator extends Generator {
     }
     return output.join('\n');
   }
+}
+
+// Runs for anything annotated as deprecated
+class DeprecatedGeneratorForAnnotation
+    extends GeneratorForAnnotation<Deprecated> {
+  @override
+  String generateForAnnotatedElement(Element element, _, __) =>
+      '// "$element" is deprecated!';
 }
