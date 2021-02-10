bc861075e7d9e8bd229889a3926c7750302bb76b
Nate Bosch <nbosch@google.com>
2021-02-10 13:42:24 -0800

    Fix deprecation, add more pub overrides

--- source_gen/lib/src/library.dart
+++ source_gen/lib/src/library.dart
@@ -38,7 +38,7 @@ class LibraryReader {
     for (var cu in element.units) {
       yield* cu.accessors;
       yield* cu.enums;
-      yield* cu.functionTypeAliases;
+      yield* cu.typeAliases;
       yield* cu.functions;
       yield* cu.mixins;
       yield* cu.topLevelVariables;
